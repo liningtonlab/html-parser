108968f5438734e0705974b147590be5a1af747b
A-Maras <72049888+A-Maras@users.noreply.github.com>
2020-10-14 12:00:15 -0700

    change .num_columns/.get columns

--- tests/test_sample.py
+++ tests/test_sample.py
@@ -13,10 +13,10 @@ a_variable = souping.inputs(r'C:\Users\maras\Desktop\JNP_html_tables\terps.html'
 
 souping.soup_id_headers(a_variable)
 
-souping.num_columns(souping.soup_id_headers(a_variable))
+souping.num_columns(souping.soup_id_headers(a_variable)) #
 
 souping.soup_comp_id(a_variable)
 
 souping.soup_id_rows(a_variable)
 
-souping.get_columns(souping.soup_id_rows(a_variable), souping.soup_id_headers(a_variable))
+souping.get_columns(souping.soup_id_rows(a_variable), souping.soup_id_headers(a_variable)) #
