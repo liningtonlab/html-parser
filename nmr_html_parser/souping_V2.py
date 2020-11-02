738a4dc38760a8c74364d8878f7c5c273814040c
A-Maras <72049888+A-Maras@users.noreply.github.com>
2020-11-02 09:31:26 -0800

    Fixing CSV output
    Need to change the format of CSV. Instead of one table atop the other,  need to have them side by side with and only atom index used once on far left. Could be done by adding all rows together for all compounds; try to make work. Use zip to iterate over the multiple lists simultaneously.

--- nmr_html_parser/souping_V2.py
+++ nmr_html_parser/souping_V2.py
@@ -142,6 +142,16 @@ def column_id_cleaner_list(d2_list):
     '''Takes 2dlist of columns . Searchs cells first for regex patterns to detect if column will contain H/C NMR, then each cell for regex patterns'''
     # Regex patterns; detect the table type to determine which column type
     # TODO: Add other possible multiplicity regex patterns
+        # 1. Other less common H splitting pattern
+        # 2. If no CH/CHn in cells(just numbers)
+        # 3. If no splitting(just numbers)
+    # TODO: Put in other cell detection methods, ie if just numbers in columns(returns blank list since no detection)
+        # 1. If headers; Can use position index in headers to determine location in columns of the data type
+        # 2. No headers;
+            # 2a. If there is other info in cells the current program can handle that
+            # 2b. If there is just numbers, then have to use get_float_averages() to obatin averages to determine type
+    # TODO: Figure out way to try each different detection method if the others fail, if all fail, return program failure
+    # TODO: ALSO if no H/C found but, the other type detected(Only one NMR type present) don't return a blank value
     CNMR_pattern_1 = re.compile(r'\,\sCH3|\,\sCH2|\,\sCH|\,\sC')
     CNMR_pattern_2 = re.compile(r'CH3|CH2|CH|C')
     regex_pattern_1 = re.compile(r'Î´C')
