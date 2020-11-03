fdc712cd07281ddbd0190dc8f24c81bbdb097e46
A-Maras <andrew_maras@sfu.ca>
2020-11-03 15:54:58 -0800

    Revert "Revert "jeff: test outline""
    This reverts commit 27df8e6958b3e1a6b4cb090ebd2b1d0b0936fb06.

--- tests/test_souping.py
+++ tests/test_souping.py
@@ -1,12 +1,27 @@
 # -*- coding: utf-8 -*-
 import pytest
 
+import pandas as pd
+from pandas.testing import assert_frame_equal
+from pathlib import Path
 from bs4 import BeautifulSoup
-from nmr_html_parser import souping
+from nmr_html_parser import souping, runner
 
-## Add some real unit tests
+# Add some real unit tests
 
-## Example
+FILES = ["test_1", "test_2"]
+
+
+def load_expected(fname):
+    fpath = Path() / "outputs" / fname
+    return pd.read_csv(fpath)
+
+
+@pytest.mark.parametrize("fname", FILES)
+def test_parse(fname):
+    expected = load_expected(fname)
+    output = runner.parse(Path() / "inputs" / fname)
+    assert_frame_equal(expected, output)
 
 
 def test_inputs():
