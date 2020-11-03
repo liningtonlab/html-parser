ebda98e3e4f62f21e55a476387e81fd2fddedfe1
A-Maras <andrew_maras@sfu.ca>
2020-11-03 15:44:55 -0800

    Attempts to add ability to handle single table types

--- main.py
+++ main.py
@@ -37,18 +37,39 @@ def main():
     print(atom_index, float_hspec, float_cspec, hmult, jcoup, ctype)
     if not float_cspec:
         columns = souping_V2.column2dlist_string_to_float(columns)
-        cspec,hspec = souping_V2.get_float_avg(columns)
-        float_cspec = cspec
+        cspec1,hspec1 = souping_V2.get_float_avg(columns)
+        float_cspec = cspec1
         ctype = souping_V2.blanks_list(float_cspec)
-    if not float_hspec:
+    elif not float_hspec:
         columns = souping_V2.column2dlist_string_to_float(columns)
-        cspec, hspec = souping_V2.get_float_avg(columns)
-        float_hspec = hspec
+        cspec1, hspec1 = souping_V2.get_float_avg(columns)
+        float_hspec = hspec1
         hmult,jcoup = souping_V2.blanks_list(float_hspec) # might have to make variables separate
     print(atom_index, float_hspec, float_cspec, hmult, jcoup, ctype)
     # Turn compound tables into CSV
+    #souping_V2.tableto_csv(*souping_V2.data_to_grid_Ca(compound_num, atom_index, float_hspec, hmult, jcoup))
+    #  TODO: def function for each H/C if only one type
+   # if not float_cspec or ctype:
+        #souping_V2.tableto_csv(*souping_V2.data_to_grid_Ca(compound_num, atom_index, float_hspec, hmult, jcoup))
+        # Means have neither cspec or ctype after using float avg search. Add everything but float_cpsec/ctype
+   # if not ctype:
+        #souping_V2.tableto_csv(*souping_V2.data_to_grid_Cb(compound_num,float_cspec, atom_index, float_hspec, hmult, jcoup))
+        # Means have float_cspec, but not ctype. Add everything but ctype
 
-    souping_V2.tableto_csv(*souping_V2.data_to_grid(compound_num, atom_index, float_cspec, ctype, float_hspec, hmult, jcoup))
+        # Need two functions, one for each case that takes the according amount of variable to add to CSV
+    # Then the cases for Proton
+    #if not float_hspec:
+        # all but float_hspec,hmulti,jcoup
+    #if not hmulti:
+        #all but hmulti
+   # if not jcoup:
+        #all but jcoup
+    #if not hmulti and jcoup:
+        # all but hmulti,jcoup
+
+        # Need four functions, one for each case
+    if float_cspec and float_hspec:
+        souping_V2.tableto_csv(*souping_V2.data_to_grid(compound_num, atom_index, float_cspec, ctype, float_hspec, hmult, jcoup))
 
 # Best practice to use this for scripts
 if __name__ == "__main__":
