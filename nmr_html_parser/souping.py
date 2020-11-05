2365356f6ff7bfe4dc2a9ff491c256d59274fe09
jvansan <jeffreyavansanten@gmail.com>
2020-11-05 11:34:55 -0800

    jeff fixes: testing paths

--- nmr_html_parser/souping.py
+++ nmr_html_parser/souping.py
@@ -1,24 +1,26 @@
 # -*- coding: utf-8 -*-
-'''
+"""
 Created on Fri Oct  9 16:04:54 2020
 
-@author: maras'''
+@author: maras"""
 
 from pathlib import Path
 from bs4 import BeautifulSoup
 import re
 import csv
 
+
 def inputs(filepath):
-    '''Takes filepath as input and returns BeautifulSoup object'''
+    """Takes filepath as input and returns BeautifulSoup object"""
     inp_file1 = Path(filepath)  # UTF-8
     with inp_file1.open() as f:
         f = f.read()
         # TODO: Ensure this is working properly to clear junk; check other parts that used I^ in search b/c now δ
-        f = str(f).replace('&nbsp;', ' ')
-        f = f.encode('cp1252')
+        f = str(f).replace("&nbsp;", " ")
+        f = f.encode("cp1252")
         soup = BeautifulSoup(f, "lxml")
     return soup
+
 
 # Simple Functions
 def num_columns(headers):
@@ -40,54 +42,57 @@ def no_space_2dlist(list_list):
 
 
 def cell_clean(i):
-    '''Takes string converting to text, removing line breaks/empty elements, strips extra whitespace'''
+    """Takes string converting to text, removing line breaks/empty elements, strips extra whitespace"""
     return i.text.replace("\n", "").strip()
 
 
 def all_same(items):
-    '''Takes list and checks if all the elements in said list are the same, returning True if so'''
+    """Takes list and checks if all the elements in said list are the same, returning True if so"""
     return all(map(lambda x: x == items[0], items))
+
 
 def blanks_list(list_length):
     blanks = []
     for i in list_length:
         blanks1 = []
         for x in range(len(i)):
-            blanks1.append(r'')
+            blanks1.append(r"")
         blanks.append(blanks1)
     return blanks
 
 
-
 # html Table Parsing Functions
 def soup_id_headers(soup):
-    '''Takes soup object and returns column headers'''
+    """Takes soup object and returns column headers"""
     # if soup.find_all("th", class_="colsep0 rowsep0"):
     header_1 = [cell_clean(i) for i in soup.find_all("th", class_="colsep0 rowsep0")]
     return no_space_list(header_1)
 
 
 def soup_comp_id(soup):
-    '''Takes soup object and returns compound identification headers'''
+    """Takes soup object and returns compound identification headers"""
     header_1 = [cell_clean(i) for i in soup.find_all("th", class_="rowsep1 colsep0")]
     return header_1
 
 
 def soup_id_rows(soup):
-    '''Takes soup object and returns rows'''
-    rows = [[cell_clean(j) for j in i.find_all("td")] for i in soup.tbody.find_all("tr")]
+    """Takes soup object and returns rows"""
+    rows = [
+        [cell_clean(j) for j in i.find_all("td")] for i in soup.tbody.find_all("tr")
+    ]
     return rows
 
 
 def compound_number(compounds, headers):
-    '''Takes primary headers and compound id headers and returns the number of compounds.
-    Based on len of compound id headers, numbers in main headers or number of hits of IH/IC'''
+    """Takes primary headers and compound id headers and returns the number of compounds.
+    Based on len of compound id headers, numbers in main headers or number of hits of IH/IC"""
     if compounds:  # 1
         return len(compounds)
     elif any("1" or "2" in s for s in headers):  # 2 Maybe use regex
         return len(headers) - 1
-    elif any("Î´C" or "Î´H" in s for s in
-             headers):  # 3;  Change to regex pattern (Could have spaces, H/C in one, different characters)
+    elif any(
+        "Î´C" or "Î´H" in s for s in headers
+    ):  # 3;  Change to regex pattern (Could have spaces, H/C in one, different characters)
         search = ["Î´H", "Î´C"]
         result = {k: 0 for k in search}
         for item in headers:
@@ -103,46 +108,50 @@ def compound_number(compounds, headers):
 
 
 def get_columns(rows, headers):
-    '''Takes rows and length of headers to get columns based on 2D list index from rows'''
+    """Takes rows and length of headers to get columns based on 2D list index from rows"""
     columns = [[x[j] for x in rows] for j in range(len(headers))]
     return columns
 
 
 def get_atom_index_column(columns):
-    '''Enumerate the list of columns so that positional index and atom_index can be returned'''
+    """Enumerate the list of columns so that positional index and atom_index can be returned"""
     return list(enumerate(columns))[0]
     # atom_index should be first column so can take that list and go from there
 
 
 def table_detect(soup, d2list, float_d2list):
-    '''Takes soup object, 2dlist of column cells, 2dlist of cell floats. Uses regex/string arguments and calculates float averages to detect and return table type'''
+    """Takes soup object, 2dlist of column cells, 2dlist of cell floats. Uses regex/string arguments and calculates float averages to detect and return table type"""
     # TODO: If use this, can do soup.find("th",string=re.compile("δ")).find("sub",string=re.compile("C"))
-    Carbon = soup.find("sub",
-                       string=re.compile("C"))  # Don't use findall, look in through th for <sub> w/ string='C'or'H'
+    Carbon = soup.find(
+        "sub", string=re.compile("C")
+    )  # Don't use findall, look in through th for <sub> w/ string='C'or'H'
     Proton = soup.find("sub", string=re.compile("H"))
     if Carbon and Proton:
-        return ('Both H1/C13 NMR Table Detected!')
+        return "Both H1/C13 NMR Table Detected!"
     elif Carbon:
-        return ('C13 NMR Table Detected!')
+        return "C13 NMR Table Detected!"
     elif Proton:
-        return (('H1 NMR Table Detected!'))
+        return "H1 NMR Table Detected!"
     else:
         for item in d2list:
             for value in item:
                 if re.search(
-                        r'(\d*[0-9]\.\d*[0-9]\,\s{1}\w*[s,t,d,m,q,b,r]\s{1})|(\([0-9]+\.[0-9]\)|\([0-9]+\.[0-9](?:\,\s{1}[0-9]+\.[0-9])*\))',
-                        value):
+                    r"(\d*[0-9]\.\d*[0-9]\,\s{1}\w*[s,t,d,m,q,b,r]\s{1})|(\([0-9]+\.[0-9]\)|\([0-9]+\.[0-9](?:\,\s{1}[0-9]+\.[0-9])*\))",
+                    value,
+                ):
                     HNMR_Search = True
-                elif re.search(r'(\d*[0-9]\.\d*[0-9])(\,\sCH3|\,\sCH2|\,\sCH|\,\sC)', value):
+                elif re.search(
+                    r"(\d*[0-9]\.\d*[0-9])(\,\sCH3|\,\sCH2|\,\sCH|\,\sC)", value
+                ):
                     CNMR_Search = True
                 else:
                     continue
         if HNMR_Search and CNMR_Search:
-            return ("Both H1/C13 NMR Detected! - From Cells!")
+            return "Both H1/C13 NMR Detected! - From Cells!"
         elif HNMR_Search and not CNMR_Search:
-            return ("H1 NMR Detected! -  From Cells!")
+            return "H1 NMR Detected! -  From Cells!"
         elif CNMR_Search and not HNMR_Search:
-            return ('H1 NMR Table Detected! - From cells')
+            return "H1 NMR Table Detected! - From cells"
         else:
             average_list = []
             for item in float_d2list:
@@ -161,37 +170,40 @@ def table_detect(soup, d2list, float_d2list):
                         HNMR = True
                         continue
             if CNMR and HNMR == True:
-                return ("Both H1/C13 NMR Detected! - From chemical shifts!")
+                return "Both H1/C13 NMR Detected! - From chemical shifts!"
             elif HNMR and not CNMR:
-                return ("H1 NMR Detected! -  From chemical shifts!")
+                return "H1 NMR Detected! -  From chemical shifts!"
             elif CNMR and not HNMR:
-                return ('H1 NMR Table Detected! - From chemical shifts!')
+                return "H1 NMR Table Detected! - From chemical shifts!"
             else:
                 return None
 
 
 def column_id_cleaner_list(d2_list):
-    '''Takes 2dlist of columns . Searchs cells first for regex patterns to detect if column will contain H/C NMR, then each cell for regex patterns'''
+    """Takes 2dlist of columns . Searchs cells first for regex patterns to detect if column will contain H/C NMR, then each cell for regex patterns"""
     # Regex patterns; detect the table type to determine which column type
     # TODO: Add other possible multiplicity regex patterns
     # 1. Other less common H splitting pattern
 
-
-    CNMR_pattern_1 = re.compile(r'\,\sCH3|\,\sCH2|\,\sCH|\,\sC')
-    CNMR_pattern_2 = re.compile(r'CH3|CH2|CH|C')
-    regex_pattern_1 = re.compile(r'Î´C')
-    regex_pattern_2 = re.compile(r'Î´H')
+    CNMR_pattern_1 = re.compile(r"\,\sCH3|\,\sCH2|\,\sCH|\,\sC")
+    CNMR_pattern_2 = re.compile(r"CH3|CH2|CH|C")
+    regex_pattern_1 = re.compile(r"Î´C")
+    regex_pattern_2 = re.compile(r"Î´H")
     HNMR_pattern_1 = re.compile(
-        r'(\,{1}\s\w*[stdmqbrqh]\s?\w*[stdmqbrqh]\s?|\,{1}\s\w*[stdmqbrqh]\s?|\([0-9]+\.[0-9]\)|\([0-9]+\.[0-9](?:\,\s{1}[0-9]+\.[0-9])*\,?\))')
+        r"(\,{1}\s\w*[stdmqbrqh]\s?\w*[stdmqbrqh]\s?|\,{1}\s\w*[stdmqbrqh]\s?|\([0-9]+\.[0-9]\)|\([0-9]+\.[0-9](?:\,\s{1}[0-9]+\.[0-9])*\,?\))"
+    )
     HNMR_pattern_2 = re.compile(
-        r'(\s?\w*[stdmqbrqh]\s?\w*[stdmqbrqh]\s?\([0-9]+\.[0-9]\)|\s?\w*[stdmqbrqh]\s?\([0-9]+\.[0-9](?:\,\s?[0-9]+\.[0-9])*\,?\)|\s?\w*[stdmqbrqh]\s?\w*[stdmqbrqh]\s?\([0-9]+\.[0-9](?:\,\s?[0-9]+\.[0-9])*\,?\)|\s?\w*[stdmqbrqh]\s?\w*[stdmqbrqh]\s?|\s?\w*[stdmqbrqh]\s?)')
-    HNMR_pattern_2a = re.compile(r'(\s?\w*[stdmqbrqh]\s?\w*[stdmqbrqh]\s?$|\s?\w*[stdmqbrqh]\s?$)')
+        r"(\s?\w*[stdmqbrqh]\s?\w*[stdmqbrqh]\s?\([0-9]+\.[0-9]\)|\s?\w*[stdmqbrqh]\s?\([0-9]+\.[0-9](?:\,\s?[0-9]+\.[0-9])*\,?\)|\s?\w*[stdmqbrqh]\s?\w*[stdmqbrqh]\s?\([0-9]+\.[0-9](?:\,\s?[0-9]+\.[0-9])*\,?\)|\s?\w*[stdmqbrqh]\s?\w*[stdmqbrqh]\s?|\s?\w*[stdmqbrqh]\s?)"
+    )
+    HNMR_pattern_2a = re.compile(
+        r"(\s?\w*[stdmqbrqh]\s?\w*[stdmqbrqh]\s?$|\s?\w*[stdmqbrqh]\s?$)"
+    )
 
     HNMR_pattern_2b = re.compile(
-        r'(\s\w*[stdmqbrqh]\s?\w*[stdmqbrqh]\s?\([0-9]+\.[0-9]\)|\s\w*[stdmqbrqh]\s?\([0-9]+\.[0-9](?:\,\s?[0-9]+\.[0-9])*\)|\s\w*[stdmqbrqh]\s?\w*[stdmqbrqh]\s?\([0-9]+\.[0-9](?:\,\s?[0-9]+\.[0-9])*\))')
-    HNMR_pattern_2ba = re.compile(
-        r'(?<=\()([^\)]+)(?=\))')
-    HNMR_pattern_2bb = re.compile(r'(\w*[stdmqbrqh]\s?\w*[stdmqbrqh]|\w*[stdmqbrqh])')
+        r"(\s\w*[stdmqbrqh]\s?\w*[stdmqbrqh]\s?\([0-9]+\.[0-9]\)|\s\w*[stdmqbrqh]\s?\([0-9]+\.[0-9](?:\,\s?[0-9]+\.[0-9])*\)|\s\w*[stdmqbrqh]\s?\w*[stdmqbrqh]\s?\([0-9]+\.[0-9](?:\,\s?[0-9]+\.[0-9])*\))"
+    )
+    HNMR_pattern_2ba = re.compile(r"(?<=\()([^\)]+)(?=\))")
+    HNMR_pattern_2bb = re.compile(r"(\w*[stdmqbrqh]\s?\w*[stdmqbrqh]|\w*[stdmqbrqh])")
 
     C_type = []
     Carbon_spec = []
@@ -204,14 +216,21 @@ def column_id_cleaner_list(d2_list):
         H_multiplicity_J1 = []
 
         for value in item:
-            if CNMR_pattern_1.search(value):  # if CNMR_pattern_1 found with .search regex:
-                c_type1.append(CNMR_pattern_2.search(value).group())  # append item to new list
-                Carbon_spec1.append(CNMR_pattern_1.sub("",
-                                                       value))  # while removing from original by adding everything but pattern to new list
+            if CNMR_pattern_1.search(
+                value
+            ):  # if CNMR_pattern_1 found with .search regex:
+                c_type1.append(
+                    CNMR_pattern_2.search(value).group()
+                )  # append item to new list
+                Carbon_spec1.append(
+                    CNMR_pattern_1.sub("", value)
+                )  # while removing from original by adding everything but pattern to new list
             elif HNMR_pattern_1.search(value):  # Same as CNMR, but for HNMR
                 H_multiplicity_J1.append(HNMR_pattern_2.search(value).group())
                 H_spec1.append(HNMR_pattern_1.sub("", value))
-            elif "" == value:  # elif " "(blank space, could be from regex search; append to list, but keep in original
+            elif (
+                "" == value
+            ):  # elif " "(blank space, could be from regex search; append to list, but keep in original
                 c_type1.append(value)
                 Carbon_spec1.append(value)
                 H_spec1.append(value)
@@ -250,11 +269,11 @@ def column_id_cleaner_list(d2_list):
 
 
 def column2dlist_string_to_float(d2_list):
-    ''' Takes 2dlist that has been cleaned by column_id_cleaner()(or any dictionary), will just take decimal number
+    """ Takes 2dlist that has been cleaned by column_id_cleaner()(or any dictionary), will just take decimal number
     if string begins with it (regex pattern(^\d*[0-9].{1}\d*[0-9]) recognized) and convert to float in a new list
     INPUT: 2dlist
-    OUTPUT: returns 2dlist with float numbers if regex pattern recognized'''
-    float_pattern = re.compile(r'(^\d*[0-9].{1}\d*[0-9])')
+    OUTPUT: returns 2dlist with float numbers if regex pattern recognized"""
+    float_pattern = re.compile(r"(^\d*[0-9].{1}\d*[0-9])")
     new_result = []
     for item in d2_list:
         spec_float1 = []
@@ -286,6 +305,7 @@ def data_to_grid(numcomps, aindex, cspec, ctype, hspec, hmult, hcoup):
         data.extend([cspec[j], ctype[j], hspec[j], hmult[j], hcoup[j]])
     return headers, data
 
+
 def data_to_grid_Ca(numcomps, aindex, hspec, hmult, hcoup):
     headers = ["atom_index"]
     data = [aindex]
@@ -296,6 +316,8 @@ def data_to_grid_Ca(numcomps, aindex, hspec, hmult, hcoup):
     for j in range(numcomps):
         data.extend([hspec[j], hmult[j], hcoup[j]])
     return headers, data
+
+
 def data_to_grid_Cb(numcomps, aindex, cspec, hspec, hmult, hcoup):
     headers = ["atom_index"]
     data = [aindex]
@@ -304,8 +326,10 @@ def data_to_grid_Cb(numcomps, aindex, cspec, hspec, hmult, hcoup):
         hl = hstring.format(i).split(",")
         headers.extend(hl)
     for j in range(numcomps):
-        data.extend([cspec[j],hspec[j], hmult[j], hcoup[j]])
+        data.extend([cspec[j], hspec[j], hmult[j], hcoup[j]])
     return headers, data
+
+
 #  TODO: def function for each H/C if only one type
 def data_to_grid_Ha(numcomps, aindex, cspec, ctype):
     headers = ["atom_index"]
@@ -318,6 +342,7 @@ def data_to_grid_Ha(numcomps, aindex, cspec, ctype):
         data.extend([cspec[j], ctype[j]])
     return headers, data
 
+
 def data_to_grid_Hb(numcomps, aindex, cspec, ctype, hspec, hcoup):
     headers = ["atom_index"]
     data = [aindex]
@@ -328,6 +353,7 @@ def data_to_grid_Hb(numcomps, aindex, cspec, ctype, hspec, hcoup):
     for j in range(numcomps):
         data.extend([cspec[j], ctype[j], hspec[j], hcoup[j]])
     return headers, data
+
 
 def data_to_grid_Hc(numcomps, aindex, cspec, ctype, hspec):
     headers = ["atom_index"]
@@ -340,6 +366,7 @@ def data_to_grid_Hc(numcomps, aindex, cspec, ctype, hspec):
         data.extend([cspec[j], ctype[j], hspec[j]])
     return headers, data
 
+
 def data_to_grid_Hd(numcomps, aindex, cspec, ctype, hspec, hmult):
     headers = ["atom_index"]
     data = [aindex]
@@ -351,9 +378,10 @@ def data_to_grid_Hd(numcomps, aindex, cspec, ctype, hspec, hmult):
         data.extend([cspec[j], ctype[j], hspec[j], hmult[j]])
     return headers, data
 
+
 def tableto_csv(headers, data):
     rows = zip(*data)
-    with open('html_parse_output.csv', 'w', encoding="UTF-8", newline='') as myfile:
+    with open("html_parse_output.csv", "w", encoding="UTF-8", newline="") as myfile:
         wr = csv.writer(myfile, quoting=csv.QUOTE_ALL)
         wr.writerow(headers)
         for row in rows:
@@ -377,4 +405,5 @@ def get_float_avg(dict2):
                 cspec.append(list(item))
             elif 0.0 <= average <= 13.5:
                 hspec.append(list(item))
-    return cspec,hspec
+    return cspec, hspec
+
--- tests/test_souping.py
+++ tests/test_souping.py
@@ -8,25 +8,33 @@ from bs4 import BeautifulSoup
 from nmr_html_parser import souping, runner
 
 # Add some real unit tests
+TESTDIR = Path(__file__).parent
 
-FILES = ["test_1", "test_BOTH_double_ai","test_Both_no_CHn","test_Both_no_hmult_jcoup","test_CNMR_only","test_HMNR_ONLY"]
+FILES = [
+    "test_1",
+    "test_BOTH_double_ai",
+    "test_Both_no_CHn",
+    "test_Both_no_hmult_jcoup",
+    "test_CNMR_only",
+    "test_HMNR_ONLY",
+]
 
 
 def load_expected(fname):
-    fpath = Path() / "outputs" / fname
+    fpath = TESTDIR / "outputs" / fname
     return pd.read_csv(fpath)
 
 
 @pytest.mark.parametrize("fname", FILES)
 def test_parse(fname):
-    expected = load_expected(fname)
-    output = runner.parse(Path() / "inputs" / fname)
+    expected = load_expected(fname + ".csv")
+    output = runner.parse(TESTDIR / "inputs" / f"{fname}.html")
     assert_frame_equal(expected, output)
 
 
 def test_inputs():
     # Need to make this better
-    soup = souping.inputs("./tests/test_table.html")
+    soup = souping.inputs(TESTDIR / "test_table.html")
     assert isinstance(soup, BeautifulSoup)
     assert soup.prettify()
     # This next line causes test to fail on purpose as a demonstration
