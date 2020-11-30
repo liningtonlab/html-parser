4bccd8be00028e26d0d878120ecb0a656c5ae657
A-Maras <andrew_maras@sfu.ca>
2020-11-30 15:33:10 -0800

    CSV to JSON Progress
    Need to clean/finish want to save before continuing.

---
+++ csv_json_main.py
@@ -0,0 +1,2 @@
+from nmr_html_parser import csv_to_json
+
--- nmr_html_parser/csv_to_json.py
+++ nmr_html_parser/csv_to_json.py
@@ -1,11 +1,20 @@
 import csv, json
 
-# Filepaths
+
+# File_paths
 csvFilePath = "table_example.csv"
 jsonFilePath = "happy.json"
 
 
+def no_blank(input_string):
+    if input_string == "":
+        return False
+    else:
+        return True
+
+
 # CSV reformatting to structure for JSON
+#def input_file(filepath)
 with open(csvFilePath, encoding="UTF-8") as csvFile:
     csvReader = csv.DictReader(csvFile)
     new_dict = {}
@@ -15,54 +24,7 @@ with open(csvFilePath, encoding="UTF-8") as csvFile:
                 new_dict[k] = new_dict[k] + [v]
             else:
                 new_dict[k] = [v]
-    #print(new_dict)  # New dictionary created
-
-    '''atom_index': ['2', '3', '4', '4a', '5', '6', '7', '8', '8a', '9', '10', '11', '', '12', '13', '14', '15', '2′',
-                   '3′', '4′', '4a′', '5′', '6′', '7′', '8′', '8a′', '9′', '10′', '11′', '', '12′', '13′', '14′',
-                   '15′',
-                   'OCH3-5'],
-    '1_cspec': ['118.6', '117.5', '125.0', '108.0', '144.0', '113.5', '149.1', '128.1', '141.7', '143.7', '105.0',
-                '69.9', '', '120.4', '137.8', '17.9', '25.7', '161.0', '112.6', '139.0', '107.1', '148.4', '114.2',
-                '158.1', '94.6', '152.5', '145.0', '104.9', '72.6', '', '82.8', '82.5', '22.8', '29.1', '60.9'],
-    '1_hspec': ['', '5.73', '7.22', '', '', '', '', '', '', '7.51', '6.86', '4.53', '', '5.3', '', '1.54', '1.59',
-                '',
-                '6.1', '8.06', '', '', '', '', '7.15', '', '7.59', '7.1', '4.86', '', '4.44', '', '1.65', '1.55',
-                '4.04'],
-    '1_multi': ['', 'd', 'd', '', '', '', '', '', '', 'd', 'd', 'd', '', 't', '', 's', 's', '', 'd', 'd', '', '',
-                '', '',
-                'brs', '', 'd', 'dd', 'd', '', 't', '', 's', 's', 's'],
-    '1_coupling': ['', '9.9', '9.9', '', '', '', '', '', '', '2.2', '2.2', '7.0', '', '7.0', '', '', '', '', '9.8',
-                   '9.8', '', '', '', '', '', '', '2.4', '2.4, 1.0', '6.4', '', '6.4', '', '', '', ''],
-    '2_cspec': ['117.6', '117.0', '125.4', '108.0', '144.2', '113.3', '149.6', '128.2', '142.0', '143.8', '104.9',
-                '70.2', '', '120.5', '138.5', '18.1', '25.8', '160.9', '113.2', '138.9', '107.3', '148.1', '114.0',
-                '158.1', '95.0', '152.6', '145.3', '104.6', '71.3', '', '81.1', '82.0', '22.8', '27.8', '60.9'],
-    '2_hspec': ['', '5.67', '7.24', '', '', '', '', '', '', '7.52', '6.87', '4.71', '4.73', '5.6', '', '1.69',
-                '1.75',
-                '', '6.2', '8.12', '', '', '', '', '7.2', '', '7.6', '6.95', '4.57', '4.54', '4.79', '', '1.35',
-                '1.71',
-                '4.05'],
-    '2_multi': ['', 'd', 'd', '', '', '', '', '', '', 'd', 'd', 'dd', 'dd', 't', '', 's', 's', '', 'd', 'd', '', '',
-                '',
-                '', 'brs', '', 'd', 'dd', 'dd', 'dd', 'dd', '', 's', 's', 's'],
-    '2_coupling': ['', '9.9', '9.9', '', '', '', '', '', '', '2.1', '2.1', '11.3, 7.0', '11.3, 7.0', '7.0', '', '',
-                   '',
-                   '', '9.8', '9.8', '', '', '', '', '', '', '2.4', '2.4, 0.9', '10.2, 6.4', '10.2, 5.4',
-                   '6.4, 5.4', '',
-                   '', '', '']}'''
-
-    '''"spectrum": [
-    {
-      "shift": 4.76,
-      "atom_index": 2,
-      "multiplicity": "d",
-      "coupling": [
-        9.8
-      ]
-    },'''
-
-
-    # Split up the data for each compound
-    # Since no multi-ai, same residues and atom index for each compound
+    #  print(new_dict)  # New dictionary created
 
 
     def dictionary_parser(num_comps):
@@ -74,19 +36,52 @@ with open(csvFilePath, encoding="UTF-8") as csvFile:
                 str(i) + "_multi",
                 str(i) + "_coupling"]
 
-            found_variables = {key: val for key, val in new_dict.items() if key in possible_variables and bool(new_dict.get(k))}
+            found_variables = {
+                key: val for key, val in new_dict.items() if key in possible_variables and bool(new_dict.get(k))}
+
             comps_shift_data["compound_"+str(i)] = found_variables
         return comps_shift_data
 
 
-    shift_comps = dictionary_parser(2)
-    print(shift_comps)
+    comps_data = dictionary_parser(2)
+
+
+    #  Collect nmr data together
+    def c_nmr_shift_creator(shifts_list, atom_input_list):
+        return [({"shift": shift, "atom_index": atom})
+                for shift, atom in zip(shifts_list, atom_input_list) if no_blank(shift)]
+
+    def h_nmr_shift_multi_coup_creator(shifts_list, multi_list, coup_list, atom_input_list):
+        return [({"shift": shift, "atom_index": atom, "multiplicity": multi, "coupling": coup})
+                for shift, atom, multi, coup in zip(shifts_list, multi_list, coup_list, atom_input_list)
+                if no_blank(shift)]
+
+
+    #if new_dict["residues"]:
+    #else: new_dict["atom_index"]
+    #print(shift_creator(shift_comps["compound_"+str(index+1)][str(index+1)+"_cspec"], new_dict["atom_index"]))
+
+    # New Nested template dictionary for each compound, that becomes list of dictionaries in the end
+    list_comp_dictionary = []
+    # iterating over each compound
+    for index, (kk, vv) in enumerate(comps_data.items()):
+        comp_dictionary = {}
+        comp_dictionary["name"] = kk
+        comp_dictionary["c_nmr"] = {"spectrum":c_nmr_shift_creator(comps_data["compound_" + str(index + 1)][str(index + 1) + "_cspec"], new_dict["atom_index"])}
+        comp_dictionary["h_nmr"] = {"spectrum":h_nmr_shift_multi_coup_creator(
+            comps_data["compound_" + str(index + 1)][str(index + 1) + "_cspec"],
+            new_dict["atom_index"],
+            comps_data["compound_" + str(index + 1)][str(index + 1) + "_multi"],
+            comps_data["compound_" + str(index + 1)][str(index + 1) + "_coupling"])}
+
+        list_comp_dictionary.append(comp_dictionary)
+
+    print(list_comp_dictionary)
 
 
     # Structured format for JSON output (list of dictionaries)
     data = [
-        {
-            "name": "Compound 1",
+        {"name": "Compound 1",
             "smiles": None,
             "c_nmr": {
                 "solvent": None,
@@ -151,7 +146,10 @@ with open(csvFilePath, encoding="UTF-8") as csvFile:
          }
          }
     ]
+
+
+    # JSON dump
     with open("table_test_output.json", "w") as f:
         # json.dump(data, f)
         # nicer output
-        f.write(json.dumps(data, indent=2))
+        f.write(json.dumps(data, indent=2))  # indent=2 instead of just f.
