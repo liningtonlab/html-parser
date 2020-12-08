1bcabe4233f52747cf6be69d0746fdaee73070aa
A-Maras <andrew_maras@sfu.ca>
2020-12-08 15:14:10 -0800

    Create elsevier_parser_main.py
    Trying to figure out iterating over the child tags. For headers, use parent tag <thead>. For cells, use <tbody>.
    
    Also, trying to figure out: handling multi-cell data(will current column_clean work on ;? and it should work on 1.20-1.20), problem similar to rowspan=2(td now a parent tag that contains contains child tag of <br> with second value).
    
    Should be able to just change 3 functions in souping.py: soup_id_headers(), soup_comp_id(), soup_id_rows(). Might have to change compound_number(), inputs()
    
    Then making a main.py file for this new parsing to try and get it working. Currently trying to figure out iterating over each child tag in a parent tag using .contents() and getting into the same list of rows in the table.

---
+++ elsevier_parser_main.py
@@ -0,0 +1,76 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct 12 11:41:54 2020
+
+@author: maras
+"""
+# Use this file as a personal testing group to make sure all the functions
+# work (return what you expect) and work as part of the overall data pipeline
+
+from pathlib import Path
+from nmr_html_parser import souping
+
+def main():
+
+    # Function which takes as an input and HTML file and writes output .csv file
+    inp_file = Path("Files/html_files/123.html") # TODO: Test. Certain, string of path can be input
+                                                                        # See soupling.inputs()
+
+    # test full thing
+    # runner.parse(inp_file, "html_parse_output.csv")
+
+    # testing individual parts
+    soup = souping.inputs(inp_file)# TODO: Might not need to change
+    headers = souping.soup_id_headers(soup)# TODO:
+    rows = souping.soup_id_rows(soup)# TODO:
+    comps = souping.soup_comp_id(soup)# TODO:
+
+    # Used stored results from previous functions calls to run
+    compound_num = souping.compound_number(comps, headers)# TODO:
+
+    columns = souping.get_columns(rows, headers)# TODO: Get format of elsevier into input arguments for get_columns
+    atom_index, atom_col_index = souping.get_atom_index(columns, headers)
+    residues, residue_col_index = souping.get_residues(columns, headers)
+
+    # Remove atom_index_like from get_atom index
+    if atom_index is None and souping.atom_index_like(columns[0]):
+        atom_col_index, atom_index = 0, columns[0]
+        headers = ["no."] + headers
+        columns = souping.get_columns(rows, headers)
+
+
+    two_d_NMR_col_index = souping.is_2_d_nmr(headers)
+    ignore_cols = [atom_col_index] + two_d_NMR_col_index
+    if residue_col_index is not None:
+        ignore_cols.append(residue_col_index)
+
+    souping.fix_multidata(columns, ignore_cols)
+    float_hspec, float_cspec, hmult, jcoup, ctype = souping.column_id_cleaner_list(
+        columns, ignore_cols
+    )
+
+    souping.tableto_csv(
+        *souping.data_to_grid(
+            compound_num,
+            atom_index,
+            resi=residues,
+            cspec=float_cspec,
+            ctype=ctype,
+            hspec=float_hspec,
+            hmult=hmult,
+            hcoup=jcoup,
+        ),
+        filename="html_parse_output.csv"
+    )
+
+    # print(headers)
+    # print(comps)
+    # print(compound_num)
+    # print(columns)
+    # print(residues)
+    # print(atom_index, float_hspec, float_cspec, hmult, jcoup, ctype)
+
+
+# Best practice to use this for scripts
+if __name__ == "__main__":
+    main()
